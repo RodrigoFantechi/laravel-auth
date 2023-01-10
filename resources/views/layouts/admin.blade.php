bced988d19badef05ebb1999f12cf38a5c968801
RodrigoFantechi <rodrifante595@libero.it>
2023-01-10 17:02:36 +0100

    fix active on links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,13 +46,13 @@
                     <div class="position-sticky pt-3 sidebar-sticky">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{ url('admin') }}">
+                                <a class="nav-link {{ Route::currentRouteName() === 'admin.dashboard' ? 'active' : '' }}" aria-current="page" href="{{ url('admin') }}">
                                     <span data-feather="home" class="align-text-bottom"></span>
                                     Dashboard
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('admin.projects.index')}}">
+                                <a class="nav-link {{ Route::currentRouteName() === 'admin.projects.index' ? 'active' : '' }}" href="{{route('admin.projects.index')}}">
                                     <span data-feather="file" class="align-text-bottom"></span>
                                     Projects
                                 </a>
